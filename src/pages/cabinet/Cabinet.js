c95121864ac59d66ec663cdfee4e61303f446d59
Ihar <igorok.201367@gmail.com>
2023-05-25 00:01:37 +0300

    small changes

--- src/pages/cabinet/Cabinet.js
+++ src/pages/cabinet/Cabinet.js
@@ -11,20 +11,73 @@ import {
 
 import { useEffect, useState, useForm } from "react";
 import { useSelector, useDispatch } from 'react-redux';
-// import { Tooltip, DatePicker, Input, Select, Radio, Form, Modal, Button, Skeleton, Dropdown, Popconfirm } from "antd"
-// import { LoadingOutlined, UserOutlined, InfoCircleOutlined, SearchOutlined, InfoOutlined, CheckOutlined, DeleteOutlined, CloseOutlined } from '@ant-design/icons';
 
 import moment from "moment/moment";
 
 import "moment/locale/ru"
+import { Form, Tabs, Input, Button } from "antd";
 
 
 
 const Cabinet = (props) => {
 
+    const profile = useSelector(state => state.user)
+
     return (
       <div className="App">
-          
+          <div className="cabinet-page-main-block default-block">
+            <div className="cabinet-page-left-block">
+              <div className="custom-tabs">
+              <Tabs
+              tabPosition="left"
+              items={
+              [{label: "Профиль", 
+                key: 1,
+                children: (
+                <>
+                <Form layout="vertical">
+                  <Form.Item label="Имя">
+                    <Input/>
+                  </Form.Item>
+                  <Form.Item label="Фамилия">
+                    <Input/>
+                  </Form.Item>
+                  <Button>Сохранить</Button>
+                </Form>
+                </>)
+               },
+               {label: "Безопасность", key: 2, 
+                children: (
+                <>
+                <Form layout="vertical">
+                  <Form.Item label="Старый пароль">
+                    <Input/>
+                  </Form.Item>
+                  <Form.Item label="Новый пароль">
+                    <Input/>
+                  </Form.Item>
+                  <Button>Сохранить</Button>
+                </Form>
+                </>)
+               },
+               {label: "Управление пользователями", key: 3, 
+                children: (
+                <>
+                </>)
+               }
+              ]}
+              ></Tabs>
+              </div>
+            </div>
+            <div className="cabinet-page-right-block">
+              <img style={{"width": "100px"}} src="./user.png"></img>
+              <div className="cabinet-page-profile-info">
+                <span>{profile.data.name}</span>
+                <span>{profile.data.surname}</span>
+                <span>Email: {profile.data.login}</span>
+              </div>
+            </div>
+          </div>
       </div>
     );
   }
